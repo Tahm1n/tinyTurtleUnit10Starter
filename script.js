b2c2c796fd65c975e4f516a4ad83a281dba05834
Tahmin Ahmed <theosphice97@gmail.com>
2017-03-27 20:27:53 +0000

    rectangle

--- script.js
+++ script.js
@@ -11,8 +11,25 @@ forward(50);
 right(90);
 forward(50);
 }
-
+function rectangle(){
+    right(72);
+    forward(130);
+    right(72);
+    forward(130);
+    right(72);
+    forward(130);
+    right(72);
+    forward(130);
+    right(72);
+    forward(130);
+    
+  
+    
+    
+}
 // Type your function call below
 
+rectangle();
 
 stamp();
+  
